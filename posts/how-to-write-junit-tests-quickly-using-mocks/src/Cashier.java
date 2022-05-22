65243e60ec6223005ecb3870068c7d386f0db44e
Alan Tan <alantanlc@gmail.com>
2022-05-22 21:52:26 +0800

    Cashier class

---
+++ posts/how-to-write-junit-tests-quickly-using-mocks/src/Cashier.java
@@ -0,0 +1,13 @@
+public class Cashier {
+
+  private String gender;
+  
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+  
+  public String getGender() {
+    return this.gender;
+  }  
+
+}
